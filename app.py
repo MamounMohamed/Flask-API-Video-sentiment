2c6f731efd293c121c31043995a2af3d92978374
MamounMohamed <72560596+MamounMohamed@users.noreply.github.com>
2023-06-29 23:58:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,14 @@ import os
 import datetime
 
 app = Flask(__name__)
-model = load_model("Emotion.h5") 
+model_dir = 'models'
+models_list = os.listdir(model_dir)
+models_list.sort(reverse=True)
+curr_model = "Emotion.h5"
+model = load_model(curr_model)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 emotions_cnt = np.zeros(7)
-model_dir = 'models'
 @app.route('/')
 
 def home():
@@ -39,23 +42,14 @@ def upload_model():
 
 
 def get_model(model_name):
-    models_list = os.listdir(model_dir)
-    models_list.sort(reverse=True)
-    if model_name is not None :
+    
+    if model_name is not None  :
         for model_file in models_list:
             if model_file.split('_')[1]==model_name:
                 model = load_model(os.path.join(model_dir,model_file))
-                return
-
-    elif model_name is None:
-        return 
+                return model_file
     
-    else:
-        if models_list:
-            latest_model = models_list[0]
-            model = load_model(os.path.join(model_dir,latest_model))            
-        
-        
+    return curr_model
     
     
 @app.route('/upload_video', methods=['POST'])
@@ -66,8 +60,7 @@ def upload_video():
     video_path = os.path.join('uploads', video.filename)
     video.save(video_path)
     model_name = "Emotion.h5"
-    
-    get_model(model_name)
+    model_name=get_model(model_name)
     
     
     # Load the video and run emotion detection on each frame
@@ -110,7 +103,7 @@ def upload_video():
     cap.release()
     sum = np.sum(emotions_cnt)
     results = dict([(emotions[i] , emotions_cnt[i]/sum) for i in range(0,7)])
-   
+    results['model_name']=model_name
     
     return jsonify(results)
 
